0f486981c59046c64552e161437ae92cd1fbfce2
Neha14-14 <98690015+Neha14-14@users.noreply.github.com>
2022-03-22 01:10:01 +0530

    Create  Binary_Tree_Preorder_Traversal.cpp

---
+++ Milestone_4/Binary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector <int> v;
+        if(!root) return v;
+        
+        stack <TreeNode*> s;
+        
+        s.push(root);
+        
+        while(!s.empty()) {
+            TreeNode* current = s.top();
+            s.pop();
+            v.push_back(current->val);
+            
+            if(current->right) s.push(current->right);
+            
+            if(current->left) s.push(current->left);
+        }
+        return v;
+    } 
+};
