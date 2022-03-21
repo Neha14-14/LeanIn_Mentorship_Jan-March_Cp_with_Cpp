cac07eed4979ec9fa2c2191d79bcc78c73f326df
Neha14-14 <98690015+Neha14-14@users.noreply.github.com>
2022-03-22 01:12:16 +0530

    Create Binary_Tree_Inorder _Traversal.cpp

---
+++ Milestone_4/Binary_Tree_Inorder _Traversal.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode *> s;
+        TreeNode *curr = root;
+
+        while (curr != NULL || s.empty() == false)
+        {
+            
+            while (curr !=  NULL)
+            { 
+                s.push(curr);
+                curr = curr->left;
+            }
+            curr = s.top();
+            s.pop();
+
+            ans.push_back(curr->val);
+            curr = curr->right;
+        } 
+        return ans;
+    } 
+};
